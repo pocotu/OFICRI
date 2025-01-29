305ad48d021ca9642396ed586560674ce9a889fb
pocotu <learning1830@outlook.com>
2025-01-29 18:59:43 -0500

    add

--- src/js/login.js
+++ src/js/login.js
@@ -4,19 +4,29 @@ const usuarios = [
     { username: "Pedro", password: "qwerty"}
 ];
 
-document.querySelector('.login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    const username = document.querySelector('.input-field[type="text"]').value;
-    const password = document.querySelector('.input-field[type="password"]').value;
-
-    const usuarioValido = usuarios.find(u => u.username === username && u.password === password);
-
-    if (!username || !password) {
-        alert("Por favor, complete todos los campos.");
-    } else if (usuarioValido) {
-        window.location.href = "/src/pages/dashboard.html";
-    } else {
-        alert("Usario o contreña incorrectos.");
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/src/data/usuarios.json')
+    .then(response => response.json())
+    .then(usuarios => {
+        document.querySelector('.login-form').addEventListener('submit', function(event) {
+            event.preventDefault();
+        
+            const username = document.querySelector('.input-field[type="text"]').value;
+            const password = document.querySelector('.input-field[type="password"]').value;
+        
+            const usuarioValido = usuarios.find(u => u.username === username && u.password === password);
+        
+            if (!username || !password) {
+                alert("Por favor, complete todos los campos.");
+            } else if (usuarioValido) {
+                window.location.href = "/src/pages/dashboard.html";
+            } else {
+                alert("Usario o contreña incorrectos.");
+            }
+        });
+    })
+    .catch(error => {
+        console.error('Error al cargar los usuarios', error);
+    });
 });
