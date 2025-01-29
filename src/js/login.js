2e0d86d3c6e32df194ab91a2aaf2a2769837b6fc
Wally λ <59671787+pocotu@users.noreply.github.com>
2025-01-29 22:10:33 +0000

    js

---
+++ src/js/login.js
@@ -0,0 +1,22 @@
+const usuario = [
+    { username: "Juan", password: "1234"},
+    { username: "Maria", password: "abcd"},
+    { username: "Pedro", password: "qwerty"}
+];
+
+document.querySelector('.login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('.input-field[type="text"]').value;
+    const password = document.querySelector('.input-field[type="password"]').value;
+
+    const usuarioValido = usuarios.find(u => u.username === username && u.password === password);
+
+    if (!username || !password) {
+        alert("Por favor, complete todos los campos.");
+    } else if (usuarioValido) {
+        alert(`Bienvenido, ${username}!`);
+    } else {
+        alert("Usario o contreña incorrectos.");
+    }
+});
