529f840389943e41a068c2e0ee508980318bbbc8
pocotu <learning1830@outlook.com>
2025-01-28 19:02:15 -0500

    index

--- src/js/config/constants.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    API_URL: 'https://api.oficri.gob.pe',
-    AUTH_TOKEN_KEY: 'oficri_auth_token',
-    ROLES: {
-        ADMIN: 'ADMIN',
-        USER: 'USER',
-        VIEWER: 'VIEWER'
-    }
-};
