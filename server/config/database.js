62ddc09479ac410cbfd36a4de40cafc4764f66ed
rmargot02 <marrlly10@gmail.com>
2025-03-03 18:14:31 -0500

    login2

--- server/config/database.js
+++ server/config/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'OFICRI_25db',
+    password: process.env.DB_PASSWORD || '191893',
     database: process.env.DB_NAME || 'Oficri_sistema',
     waitForConnections: true,
     connectionLimit: 10,
